c5516532470e4d8e697887e0e83dc73be7ed34a9
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-24 17:04:03 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+}
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos]
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error("Empty SortedList")
+    }
+    return Math.min(...this.items);
+  }
 
-  sum() {}
+  sum() {
+    
+    if (this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((total, item) => total + item, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0){
+      throw new Error("Empty SortedList")
+    }
+    const total = this.sum();
+    return total / this.length;
+
+  }
 }
 
 module.exports = SortedList;
